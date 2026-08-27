72d5132f8a8bebec052afa0a9a3861f026f728d8
Sid Ahmed Riri <import.riri@gmail.com>
2026-08-27 10:21:03 +0200

    Start with cross-platform look and feel

--- src/app/Avvio.java
+++ src/app/Avvio.java
@@ -4,30 +4,10 @@ import app.render.QrVero;
 import app.render.SorgenteQr;
 import app.stile.Stile;
 import app.ui.Cornice;
-
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
 
 /** Punto di partenza. */
 public final class Avvio {
-
-    private Avvio() { }
-
-    public static void main(String[] args) {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ignorata) {
-            /* si prosegue col look and feel di serie: l'app resta usabile */
-        }
-        Stile.adottaFontDiSistema();
-        UIManager.put("ToolTip.font", Stile.piccolo());
-
-        final SorgenteQr qr = new QrVero();
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new Cornice(qr).setVisible(true);
-            }
-        });
-    }
+    private Avvio(){}
+    public static void main(String[] args){Stile.installaLookAndFeel();Stile.adottaFontDiSistema();final SorgenteQr qr=new QrVero();SwingUtilities.invokeLater(new Runnable(){@Override public void run(){new Cornice(qr).setVisible(true);}});}
 }
